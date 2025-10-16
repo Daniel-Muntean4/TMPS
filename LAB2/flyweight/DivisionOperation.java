3787664236824c987a3bad0c39cad0852dc3f72c
Daniel Muntean <125653791+Daniel-Muntean4@users.noreply.github.com>
2025-10-16 12:24:02 +0300

    Delete LAB2 directory

--- LAB2/flyweight/DivisionOperation.java
+++
@@ -1,9 +0,0 @@
-package LAB2.flyweight;
-
-public class DivisionOperation implements Flyweight {
-    @Override
-    public double execute(double a, double b) {
-        if (b == 0) throw new ArithmeticException("Division by zero");
-        return a / b;
-    }
-}
